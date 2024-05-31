2fd90e6481bdab386854557e88c7b78e4a92c914
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-05-31 14:05:23 +0000

    Added product views and templates

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home') # This is the route URL 
+    path('', views.all_products, name='products') 
 ]
