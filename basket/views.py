30b4860d35ad116ce686ce0c0677e9de358ed71d
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-04 12:35:57 +0000

    Added basket content and delivery logic

--- basket/views.py
+++ basket/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def view_basket(request):
-    """ A view that renders the basket content """
+    """ A view that renders the basket contents page """
+
     return render(request, 'basket/basket.html')
