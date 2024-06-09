d58227bc9ffee8cfad8b7fedd2616a26e73489ef
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-09 12:38:32 +0000

    Finished product shade logic

--- basket/views.py
+++ basket/views.py
@@ -13,12 +13,24 @@ def add_to_basket(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    shades = None
+    if 'product_shades' in request.POST:
+        shades = request.POST['product_shades']
     basket = request.session.get('basket', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity 
+    if shades:
+        if item_id in list(basket.keys()):
+            if shades in basket[item_id]['items_by_shades'].keys():
+                basket[item_id]['items_by_shades'][shades] += quantity
+            else:
+                basket[item_id]['items_by_shades'][shades] = quantity
+        else:
+            basket[item_id] = {'items_by_shades' : {shades: quantity}}
     else:
-        basket[item_id] = quantity
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity 
+        else:
+            basket[item_id] = quantity
 
     request.session['basket'] = basket
     return redirect(redirect_url)
