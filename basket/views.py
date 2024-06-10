1681eb87393f4107abb68efe034dbbdf21659346
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-10 17:47:10 +0000

    Added adjust and remove item from bag views and calculate the subtotal

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -34,3 +34,52 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ Add the quantity of the specific product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    shades = None
+    if 'product_shades' in request.POST:
+        shades = request.POST['product_shades']
+    basket = request.session.get('basket', {})
+
+    if shades:
+        if quantity > 0:
+            basket[item_id]['items_by_shades'][shades] = quantity
+        else:
+            del basket[item_id]['items_by_shades'][shades]
+            if not basket[item_id]['items_by_shades']:
+                basket.pop(item_id)
+    else:
+        if quantity > 0:
+            basket[item_id] = quantity
+        else:
+            basket.pop(items_id)        
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """ Remove item/s from the shopping bag. """
+
+    try:
+        shades = None
+        if 'product_shades' in request.POST:
+            shades = request.POST['product_shades']
+        basket = request.session.get('basket', {})
+
+        if shades:
+            del basket[item_id]['items_by_shades'][shades]
+            if not basket[item_id]['items_by_shades']:
+                basket.pop(item_id)
+        else:
+            basket.pop(item_id)        
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
