eb09f3dd8cacc8b099f31a11183cea630354d376
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-14 16:08:45 +0000

    added chekccko views and templates

--- the_beauty_box/settings.py
+++ the_beauty_box/settings.py
@@ -80,11 +80,12 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'basket.contexts.basket_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
-                'crispy_forms.templatetags.crispy_forms_fields',
+                'crispy_forms.templatetags.crispy_forms_field',
 
             ]
         },
