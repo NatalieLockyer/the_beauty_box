94fb74a3c0291bd7ed2d5b575900bb36871a21fa
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-15 09:51:45 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PE3lnRvGkLgnhhbCdwCkmCVmtlKQtrs1359FinceIGRjaDs8d8CqnCB5UdPa1bgfuykLAHsb31boH8JLad4An4g00dmIF9p5X',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
