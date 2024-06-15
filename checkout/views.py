c4086dd09840f8e09294140271e2ba2c555efa08
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-15 10:41:25 +0000

    Added pk and sk

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         message.error(request, 'There is nothing in your basket')
         return redirect(reverse('products'))
+
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    
+    print(intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
