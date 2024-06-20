06aad3b470c4d9a7f43b291e49cbd4a4bb1c6937
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-20 10:30:42 +0000

    Added profile views, forms and updated template

---
+++ profiles/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import UserProfile
+
+class UserProfileForm(forms.ModelForm):
+    """ Order form fields we want to render """
+    class Meta:
+        model = UserProfile 
+        exclude = ('user',)
+
+
+    def __init__(self, *args, **kwargs):
+        """ This will add placeholders and classes, remove auto-generated
+        labels and set autofocus on the first field. """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_town_or_city': 'Town or City',
+            'default_county': 'County, State or Locality',
+            'default_postcode': 'Postal Code',
+            'default_phone_number': 'Phone Number',    
+           }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-pink rounded-0 profile-form-input'
+            self.fields[field].label = False
