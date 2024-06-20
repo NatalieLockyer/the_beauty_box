3ce51d866291a0e3ca2fa5ed2b742aae9523a45a
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-20 11:13:44 +0000

    Linked the order number to the profile page

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from basket.contexts import basket_contents
 
 import stripe
@@ -121,17 +123,39 @@ def checkout_success(request, order_number):
     """ This will handel successful checkouts """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Your order has been successfully processed! \
-        Your Beauty Box order number is { order.order_number }. A confirmation email \
-        will be sent to { order.email }. \
-        We hope you will be pleased with your order. ')
 
-    if 'basket' in request.session:
-        del request.session['basket']
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # This will attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
 
-    template = 'checkout/checkout_success.html'
-    context = {
-        'order': order,
-    }
+        # This will save the users info
+        if save_info:
+            profile_data = {
+                'default_phone_number':  order.phone_number,
+                'default_country':  order.country,
+                'default_postcode':  order.postcode,
+                'default_town_or_city':  order.town_or_city,
+                'default_street_address1':  order.street_address1,
+                'default_street_addresss2':  order.street_address2,
+                'default_county':  order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
-    return render(request, template, context)
+        messages.success(request, f'Your order has been successfully processed! \
+            Your Beauty Box order number is { order.order_number }. A confirmation email \
+            will be sent to { order.email }. \
+            We hope you will be pleased with your order. ')
+
+        if 'basket' in request.session:
+            del request.session['basket']
+
+        template = 'checkout/checkout_success.html'
+        context = {
+            'order': order,
+        }
+
+        return render(request, template, context)
