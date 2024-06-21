4e8c788531aef02ccbb290cf08f9c5d796ff1e25
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-21 17:05:20 +0000

    Finished add product functionality

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from django.shortcuts import render, reverse, redirect, get_object_or_404
 
-from .forms import ProductForm
 from .models import Product, Category
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -72,7 +72,17 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please check the form is valid and try again.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
--- the_beauty_box/urls.py
+++ the_beauty_box/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
