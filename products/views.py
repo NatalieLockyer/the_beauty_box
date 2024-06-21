5c9fdef17ece4afaab8b3b2defedbb61d61c3b34
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-21 17:21:52 +0000

    products/

--- products/views.py
+++ products/views.py
@@ -89,3 +89,27 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST,  request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your product has been successfully updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please check the form is valid and try again')
+    else:
+        form = ProductForm(instance=product)
+    messages.info(request, f'You are editing {product.name}')
+    
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
