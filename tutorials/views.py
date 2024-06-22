7990477df349838926ad5b6d410c715b0ec92dc5
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-22 16:40:55 +0000

    Added tutorial views and templates

--- tutorials/views.py
+++ tutorials/views.py
@@ -4,8 +4,13 @@ from .models import Tutorial
 # Create your views here.
 
 def all_tutorials(request):
-    """ A view to show all tutorial videos, including sorting and search queries """
+    """ A view to show all tutorial videos """
     
     tutorials = Tutorial.objects.all()
-    
-    
+
+    context = {
+        'tutorials': tutorials,
+    }
+
+    return render(request, 'tutorials/tutorial.html', context)
+
