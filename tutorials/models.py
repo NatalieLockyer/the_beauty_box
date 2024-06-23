6d881fb9cb8059517368a47b1530e9ce0b419dc5
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-23 11:33:14 +0000

    Image added to ptutorial so this can be the display for the tutorial

--- tutorials/models.py
+++ tutorials/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Tutorial(models.Model):
     title = models.CharField(max_length=254)
     description = models.TextField()
+    image_url = models.URLField(max_length=3000, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     video_url = models.URLField(max_length=3000, null=True, blank=True)
     video = models.FileField(null=True, blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
