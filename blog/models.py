45f1297ff16f8704db13b13ddece9f3161cf70b8
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-27 16:40:56 +0000

    Added blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+class Blog(models.Model):
+    title = models.CharField(max_length=254, unique=True)
+    slug = models.SlugField(max_length=254, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
