88738ab2430abf1149e4fb8c1a5f7f94cb63fe88
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-27 16:51:49 +0000

    Added comment class in models

--- blog/models.py
+++ blog/models.py
@@ -9,5 +9,16 @@ class Blog(models.Model):
     slug = models.SlugField(max_length=254, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
+    image_url = models.URLField(max_length=3000, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
