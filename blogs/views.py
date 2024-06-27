1760793386fd086683dba12ece4e96aa9de7dcca
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-27 16:33:41 +0000

    Removed Blog App

--- blogs/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def blogs(request):
-    """ A view to return the beauty blog page """
-    return render(request, 'blogs/blogs.html')
