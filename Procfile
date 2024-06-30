48413cf4dbbdba7ed63af383cc87e8ce2a450bfe
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-30 19:23:37 +0000

    Deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn the_beauty_box.wsgi:application
--- the_beauty_box/settings.py
+++ the_beauty_box/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-i5pjsszp67k#c4$*-egd$!ep0$$fml_)nmbz2z8n#1d^d-qjl#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-natalielock-thebeautybo-wozjz2y9eye.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['NatalieLockyer-the-beauty-box.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -130,13 +130,18 @@ WSGI_APPLICATION = 'the_beauty_box.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',   
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-    
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',   
+        }
+        
+    }
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
