38bd67ead67897a8d5e39910783eee40b0b6e08c
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-06-30 18:50:14 +0000

    Create database

--- the_beauty_box/settings.py
+++ the_beauty_box/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -132,8 +133,9 @@ WSGI_APPLICATION = 'the_beauty_box.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',   
     }
+    
 }
 
 CSRF_TRUSTED_ORIGINS = [
