d139475a79478f7b75218e8b14e4421ca69a4e42
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-07-09 09:03:13 +0000

    Added app password

--- the_beauty_box/settings.py
+++ the_beauty_box/settings.py
@@ -235,7 +235,7 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Default primary key field type
